36755642952bb3a28f97e19ee82f667b3bd12450
Raul Cesar Mulerschat <raulzito737@gmail.com>
2022-03-26 13:19:30 -0300

    [M1S04] - Ex 6 - Execute o código enquanto o valor seja verdadeiro

---
+++ M1S04/isTrue.js
@@ -0,0 +1,17 @@
+
+
+let impares = 0;
+let contador = 0;
+
+
+
+
+
+while (impares < 10) {
+    contador++;
+    if(contador % 2 === 1){
+        impares++;
+    }
+    console.log("Impares :" + impares + " | "+ "contador :" + contador);
+    
+}
