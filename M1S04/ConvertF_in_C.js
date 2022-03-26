ea67d62c38bbd02d3030439bd4c5824d9e0b7d26
Raul Cesar Mulerschat <raulzito737@gmail.com>
2022-03-26 14:13:42 -0300

    [M1S04] - Ex 7 - Converter temperatura de Fahrenheit para Celsius

---
+++ M1S04/ConvertF_in_C.js
@@ -0,0 +1,15 @@
+
+
+let Fahrenheit= [ 0, 32, 45, 50, 75, 80, 99, 120 ];
+
+//Math.round( ( elem - 32 ) * 5 / 9 );
+
+
+
+
+const Celsius = Fahrenheit.map((F) => (F- 32) * 5/9);
+
+console.log(Celsius);
+
+
+
