c10c9991b7d2033bfd028adfc5b3ee96f9ec7816
Raul Cesar Mulerschat <raulzito737@gmail.com>
2022-04-13 21:58:23 -0300

    Ajustes de funcionalidas

---
+++ M1S06/[M1S06] - Ex 3 - Busca Endereço/Parte1/index.js
@@ -0,0 +1,23 @@
+
+//function fetchAddress(cep) {
+//  return fetch(`https://viacep.com.br/ws/${cep}/json/`)
+//    .then(response => response.json())
+//    .then(data => {
+//      return data;
+//    })
+//    .catch(error => {
+//      console.log(error);
+//    });
+//}
+//
+//fetchAddress('01001000')
+//  .then(data => {
+//    console.log(data);
+//  });
+
+
+
+
+
+
+
