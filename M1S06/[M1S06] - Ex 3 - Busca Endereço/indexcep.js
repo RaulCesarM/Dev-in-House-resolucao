f4f7601790ccf4e0146eed9f89b8d8a728bb3b6c
Raul Cesar Mulerschat <raulzito737@gmail.com>
2022-04-21 23:19:50 -0300

    finalizando CEP

---
+++ M1S06/[M1S06] - Ex 3 - Busca Endereço/indexcep.js
@@ -0,0 +1,36 @@
+const Rua = document.createElement("p");
+const Bairro = document.createElement("p");
+const Cidade = document.createElement("p");
+const Estado = document.createElement("p");
+
+
+var rua = "";
+var bairro = "";
+var cidade = "";
+var estado = "";
+
+async function chamaCEP() {
+    const cep = document.getElementById("InputCep").value;
+  const response = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+
+  if (!response.ok) {
+    const err = await response.json();
+    return console.log("ocorreu o seguinte erro:", err.message);
+  }
+
+  const data = await response.json();
+  rua = data.logradouro;
+  bairro = data.bairro;
+  cidade = data.localidade;
+  estado = data.uf;
+
+  Rua.innerHTML = `${rua}`;
+  Bairro.innerHTML = ` ${bairro}`;
+  Cidade.innerHTML = ` ${cidade}`;
+  Estado.innerHTML = `${estado}`;
+
+  document.getElementById("IBAIRRO").appendChild(Bairro);
+  document.getElementById("IRUA").appendChild(Rua);
+  document.getElementById("ICIDADE").appendChild(Cidade);
+  document.getElementById("IESTADO").appendChild(Estado);
+}
